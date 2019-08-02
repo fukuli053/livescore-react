bcf3341f1734c7c0a0acab97ba321ee80b0e2998
Furkan Ergün <fergun3453@hotmail.com>
2019-08-02 15:27:27 +0300

    Updates

---
+++ precache-manifest.9c4462df91c58ed9ebd9f550639f6a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d820167e7c224ea74478bee6733e970d",
+    "url": "/livescore-react/index.html"
+  },
+  {
+    "revision": "0af3794dcfcc5c8cd61d",
+    "url": "/livescore-react/static/css/2.6462ed3a.chunk.css"
+  },
+  {
+    "revision": "a7242bd4fb2256d3e4e5",
+    "url": "/livescore-react/static/css/main.64e23949.chunk.css"
+  },
+  {
+    "revision": "0af3794dcfcc5c8cd61d",
+    "url": "/livescore-react/static/js/2.6edd61e4.chunk.js"
+  },
+  {
+    "revision": "a7242bd4fb2256d3e4e5",
+    "url": "/livescore-react/static/js/main.96640a1b.chunk.js"
+  },
+  {
+    "revision": "964b6211acd29bc4b818",
+    "url": "/livescore-react/static/js/runtime~main.bb0f234e.js"
+  }
+]);
